710ea21d6ded5391c1280011f4c67b471aeb7a3d
WaleedYaser <waleedyaser95@gmail.com>
2017-03-14 00:29:20 +0200

    refactor: handle eexceptions.

--- Project/src/clientserverprogram/serverProgram.java
+++ Project/src/clientserverprogram/serverProgram.java
@@ -3,26 +3,36 @@ import java.net.*;
 
 public class serverProgram {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
-            // initialze server socket
-            ServerSocket ss = new ServerSocket(1555);
-            // wait untill client connects to the server port
-            Socket someSocket = ss.accept();
+            DataInputStream in = null;
+            DataOutputStream out = null;
             
-            // comunicate throut input/ output stream
-            InputStream is = someSocket.getInputStream();
-            DataInputStream in = new DataInputStream(is);
-            OutputStream os = someSocket.getOutputStream();
-            DataOutputStream out = new DataOutputStream(os);
+            try {
+                // initialze server socket
+                ServerSocket ss = new ServerSocket(1555);
+                // wait untill client connects to the server port
+                Socket someSocket = ss.accept();
+
+                // comunicate throut input/ output stream
+                InputStream is = someSocket.getInputStream();
+                in = new DataInputStream(is);
+                OutputStream os = someSocket.getOutputStream();
+                out = new DataOutputStream(os);
+            } catch (IOException e) {
+                System.err.println("IOException:  " + e);
+            }
             
-            while (true)
-            {
-                int number = in.readInt();
-                System.out.println("Processing " + number + " ... ");
-                out.writeInt(Square(number));
-            }	
-            //someSocket.close();
+            try {
+                while (true)
+                {
+                    int number = in.readInt();
+                    System.out.println("Processing " + number + " ... ");
+                    out.writeInt(Square(number));
+                }	
+            } catch (IOException e) {
+                System.err.println("IOException:  " + e);
+            }
 	}
         
         public static int Square(int number)
