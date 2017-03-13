f0cc30c65e0a051adb46c508940c07e8ba09ae0b
WaleedYaser <waleedyaser95@gmail.com>
2017-03-14 00:07:18 +0200

    refactor: read user input and send it to server.

--- Project/src/clientserverprogram/clientProgram.java
+++ Project/src/clientserverprogram/clientProgram.java
@@ -1,20 +1,32 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class clientProgram {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		Socket clientSocket = new Socket("localhost",1555);
-		OutputStream outStream = clientSocket.getOutputStream();
-		DataOutputStream out = new DataOutputStream(outStream);
-		out.writeUTF("This is a greeting from the client");
-		
-		InputStream inStream = clientSocket.getInputStream();
-		DataInputStream in = new DataInputStream(inStream);
-		
-		System.out.println("Server says: " + in.readUTF());	
-		clientSocket.close();
+            
+            // intialize socket to connect to the server
+            Socket clientSocket = new Socket("localhost",1555);
+            
+            // comunicate throut input/ output stream
+            InputStream inStream = clientSocket.getInputStream();
+            DataInputStream in = new DataInputStream(inStream);
+            OutputStream outStream = clientSocket.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outStream);
+            
+            while (true)
+            {
+                // read user input
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter an integer: ");
+                int number = scanner.nextInt();
+                
+                // send number to be processed
+                out.writeInt(number);
+                System.out.println("Server says: " +number +" "+ in.readInt());	
+            }
+            //clientSocket.close();
 				
 	}
 
