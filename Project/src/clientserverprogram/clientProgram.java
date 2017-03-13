6c8150a84dd778541df6ee992a154aa9b7c77287
WaleedYaser <waleedyaser95@gmail.com>
2017-03-14 01:00:04 +0200

    refactor: reading main args.

--- Project/src/clientserverprogram/clientProgram.java
+++ Project/src/clientserverprogram/clientProgram.java
@@ -6,46 +6,46 @@ public class clientProgram {
 
 	public static void main(String[] args) {
             
-            
-            Socket clientSocket = null;
             DataInputStream in = null;
             DataOutputStream out = null;
             
+            // read args from cmd
+            String host = args[0];
+            int port = Integer.parseInt(args[1]);
             try {
-            // intialize socket to connect to the server
-            clientSocket = new Socket("localhost", 1555);
-            
-            // comunicate throut input/ output stream
-            InputStream inStream = clientSocket.getInputStream();
-            in = new DataInputStream(inStream);
-            OutputStream outStream = clientSocket.getOutputStream();
-            out = new DataOutputStream(outStream);
+                // intialize socket to connect to the server
+                Socket clientSocket = new Socket(host, port);
+
+                // comunicate throut input/ output stream
+                InputStream inStream = clientSocket.getInputStream();
+                in = new DataInputStream(inStream);
+                OutputStream outStream = clientSocket.getOutputStream();
+                out = new DataOutputStream(outStream);
             } catch (UnknownHostException e) {
-                System.err.println("Don't know about host");
+                System.err.println("UnknownHostException" +  e);
             } catch (IOException e) {
-                System.err.println("Couldn't get I/O for the connection to");
+                System.err.println("IOException:  " + e);
             }
             
             try {
-            while (true)
-            {
-                // read user input
-                Scanner scanner = new Scanner(System.in);
-                System.out.print("Enter an integer: ");
-                int number = scanner.nextInt();
+                while (true)
+                {
+                    // read user input
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.print("Enter an integer: ");
+                    int number = scanner.nextInt();
+
+                    // send number to be processed
+                    out.writeInt(number);
+                    System.out.println("Square root of " + number +" is: " 
+                            + in.readInt());	
+                }
                 
-                // send number to be processed
-                out.writeInt(number);
-                System.out.println("Square root of " + number +" is: " 
-                        + in.readInt());	
-            }
             } catch (UnknownHostException e) {
                 System.err.println("Trying to connect to unknown host: " + e);
             } catch (IOException e) {
                 System.err.println("IOException:  " + e);
-            }
-            //clientSocket.close();
-				
+            }		
 	}
 
 }
